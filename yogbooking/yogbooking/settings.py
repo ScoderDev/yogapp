9f9609d303563ad0f68736408cd75308b5992626
ScoderDev <77617968+ScoderDev@users.noreply.github.com>
2021-01-18 17:37:10 +0530

    Update settings.py

--- yogbooking/yogbooking/settings.py
+++ yogbooking/yogbooking/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/static/'
+COLLECT_STATIC=False
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "yogbookingapp/static"),
